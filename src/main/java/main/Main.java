3ae97770c9d8c504b987ef66d5faaef8f7db0d83
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-07-29 23:36:00 +0300

    Moved beans annotation into xml configuration file

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,10 +4,10 @@ import enotes.postman.Postman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("config")
+@ImportResource("spring/spring-configuration.xml")
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Main.class, args);
