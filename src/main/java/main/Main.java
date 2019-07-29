1c793c0111e1f86482bb822aa5fd4c96fa1813bc
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-07-29 23:16:40 +0300

    Corrected mistake.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"enotes", "config"})
+@ComponentScan("config")
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Main.class, args);
