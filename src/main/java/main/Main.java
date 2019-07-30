1ffb100ecf16a3d49b82e0a0d4ffebfab589226c
EKiprenko <evgeniy.kiprenko@cengage.com>
2019-07-30 15:38:23 +0300

    Refactored beans config path.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("spring/spring-configuration.xml")
+@ImportResource("classpath:/spring/spring-configuration.xml")
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Main.class, args);
