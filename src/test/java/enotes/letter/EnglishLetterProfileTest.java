03b9886ef33cc7ea3c2c120b3b4dcdb07377a542
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-07-31 23:26:45 +0300

    Added completely working tests which based on Spring dependency injection.

--- src/test/java/enotes/letter/EnglishLetterProfileTest.java
+++ src/test/java/enotes/letter/EnglishLetterProfileTest.java
@@ -4,18 +4,21 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:/spring/english-spring-configuration.xml")
+@ContextConfiguration({"classpath:/spring/english-spring-configuration.xml",
+        "classpath:/spring/letter-spring-configuration.xml"})
 public class EnglishLetterProfileTest {
+
+    @Autowired
     private Letter englishLetter;
-    private final String ENGLISH_LETTER_TEXT = "Super secret text of a letter";
 
     @Test
     public void shouldReturnEnglishLetterText() {
-        String SPANISH_LETTER_TEXT = "Texto super secreto de una carta";
-        assertEquals(SPANISH_LETTER_TEXT, englishLetter.getText());
+        String ENGLISH_LETTER_TEXT = "Super secret text of a letter";
+        assertEquals(ENGLISH_LETTER_TEXT, englishLetter.getText());
     }
 }
--- src/test/java/enotes/letter/SpanishLetterProfileTest.java
+++ src/test/java/enotes/letter/SpanishLetterProfileTest.java
@@ -1,16 +1,18 @@
 package enotes.letter;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath*:spring/spanish-spring-configuration.xml")
+@ContextConfiguration({"classpath:spring/spanish-spring-configuration.xml",
+        "classpath:/spring/letter-spring-configuration.xml"})
 public class SpanishLetterProfileTest {
+
     @Autowired
     private Letter spanishLetter;
 
