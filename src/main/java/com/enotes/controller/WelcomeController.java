ffcb61d42fbde3d20ee984ff996be014b81d0fd5
EKiprenko <evgeniy.kiprenko@cengage.com>
2019-08-12 18:54:15 +0300

    Added second page and controller for it.

--- src/main/java/com/enotes/controller/WelcomeController.java
+++ src/main/java/com/enotes/controller/WelcomeController.java
@@ -1,14 +1,7 @@
 package com.enotes.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.enotes.note.Note;
-import com.enotes.note.NoteState;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -16,48 +9,9 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/")
-    public String index(Model model) {
-        LOGGER.info("Gotcha!");
-        Note note = new Note();
-        note.setState(NoteState.HIGH);
-        note.setText("Very important text, VERY!");
-        note.setHeader("Very important header.");
-
-        Note note1 = new Note();
-        note1.setState(NoteState.MIDDLE);
-        note1.setText("Not so important by be careful");
-        note1.setHeader("Middle priority header");
-
-        Note note2 = new Note();
-        note2.setState(NoteState.LOW);
-        note2.setText("Nah, take it easy, we have plenty of time");
-        note2.setHeader("Lowly header!");
-
-        Note note3 = new Note();
-        note3.setState(NoteState.MIDDLE);
-        note3.setText("Not so important by be careful");
-        note3.setHeader("Middle priority header");
-
-        Note note4 = new Note();
-        note4.setState(NoteState.LOW);
-        note4.setText("Nah, take it easy, we have plenty of time");
-        note4.setHeader("Lowly header!");
-
-        Note note5 = new Note();
-        note5.setState(NoteState.LOW);
-        note5.setText("Nah, take it easy, we have plenty of time");
-        note5.setHeader("Lowly header!");
-
-        List<Note> notes = new ArrayList<>();
-        notes.add(note);
-        notes.add(note1);
-        notes.add(note2);
-        notes.add(note3);
-        notes.add(note4);
-        notes.add(note5);
-        model.addAttribute("notes", notes);
-        LOGGER.info("request was sent");
-        return "index.html";
+    @RequestMapping({"/", "/welcome"})
+    public String welcome() {
+        LOGGER.info("Someone joined");
+        return "welcome.html";
     }
 }
