883fb6d5311283f8be6dbfd850cf4259ed939e55
EKiprenko <evgeniy.kiprenko@cengage.com>
2019-08-12 17:51:20 +0300

    Moved project from Maven to Gradle.

--- src/main/java/com/enotes/Main.java
+++ src/main/java/com/enotes/Main.java
@@ -1,7 +1,5 @@
 package com.enotes;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
