e8c1ab9b229e1a000df2c965eecd78d1cce2985f
EKiprenko <evgeniy.kiprenko@cengage.com>
2019-08-13 15:42:02 +0300

    Made simple service for notes to temporary replace DB.

--- src/main/java/com/enotes/controller/NotesCrudController.java
+++ src/main/java/com/enotes/controller/NotesCrudController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.enotes.note.Note;
 
@@ -19,5 +20,11 @@ public class NotesCrudController {
         LOGGER.info("Create page joined");
         model.addAttribute(model);
         return "notecrud/createNewNote.html";
+    }
+
+    @RequestMapping("/{id}")
+    public String viewNote(@RequestParam Integer id){
+        //get Note from DB using id;
+        return "viewNote.html";
     }
 }
--- src/main/java/com/enotes/controller/NotesGalleryViewController.java
+++ src/main/java/com/enotes/controller/NotesGalleryViewController.java
@@ -3,12 +3,15 @@ package com.enotes.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.enotes.note.Note;
 import com.enotes.note.NoteState;
+import com.enotes.note.service.NoteService;
+import com.enotes.note.service.NoteServiceSimpleImpl;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -16,54 +19,19 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class NotesGalleryViewController
 {
+
+    @Autowired
+    private NoteService noteService;
+
+    public void setNoteService(NoteService noteService)
+    {
+        this.noteService = noteService;
+    }
+
     @RequestMapping("/notesGalleryView")
     public String index(Model model) {
-        LOGGER.info("Gotcha!");
-        Note note = new Note();
-        note.setState(NoteState.HIGH);
-        note.setText("Very important text, VERY!");
-        note.setHeader("Very important header.");
-
-        Note note1 = new Note();
-        note1.setState(NoteState.MIDDLE);
-        note1.setText("Not so important by be careful");
-        note1.setHeader("Middle priority header");
-
-        Note note2 = new Note();
-        note2.setState(NoteState.LOW);
-        note2.setText("Nah, take it easy, we have plenty of time");
-        note2.setHeader("Lowly header!");
-
-        Note note3 = new Note();
-        note3.setState(NoteState.MIDDLE);
-        note3.setText("Not so important by be careful");
-        note3.setHeader("Middle priority header");
-
-        Note note4 = new Note();
-        note4.setState(NoteState.LOW);
-        note4.setText("Nah, take it easy, we have plenty of time");
-        note4.setHeader("Lowly header!");
-
-        Note note5 = new Note();
-        note5.setState(NoteState.LOW);
-        note5.setText("Nah, take it easy, we have plenty of time");
-        note5.setHeader("Lowly header!");
-
-        List<Note> notes = new ArrayList<>();
-        notes.add(note);
-        notes.add(note1);
-        notes.add(note2);
-        notes.add(note3);
-        notes.add(note4);
-        notes.add(note5);
-        notes.add(note);
-        notes.add(note1);
-        notes.add(note2);
-        notes.add(note3);
-        notes.add(note4);
-        notes.add(note5);
-        model.addAttribute("notes", notes);
-        LOGGER.info("request was sent");
+        LOGGER.info("Sending gallery view: count of notes {}", noteService.getAllNotes().size());
+        model.addAttribute("notes", noteService.getAllNotes());
         return "notesGalleryView.html";
     }
 }
