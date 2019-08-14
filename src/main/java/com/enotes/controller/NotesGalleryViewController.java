e2ca267b610f3a2f91d30abd35bccd4bd29f5419
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-08-14 23:03:54 +0300

    Added nav menu

--- src/main/java/com/enotes/controller/NotesGalleryViewController.java
+++ src/main/java/com/enotes/controller/NotesGalleryViewController.java
@@ -1,19 +1,11 @@
 package com.enotes.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.enotes.note.service.NoteService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.enotes.note.Note;
-import com.enotes.note.NoteState;
-import com.enotes.note.service.NoteService;
-import com.enotes.note.service.NoteServiceSimpleImpl;
-
-import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
