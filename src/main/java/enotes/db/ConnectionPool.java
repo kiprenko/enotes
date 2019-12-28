aac9b9a99f7e75deb68e95101882647a49e04446
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-12-28 20:21:39 +0200

    Refactored ConnectionPool

--- src/main/java/enotes/db/ConnectionPool.java
+++ src/main/java/enotes/db/ConnectionPool.java
@@ -3,10 +3,10 @@ package enotes.db;
 import java.sql.Connection;
 
 public interface ConnectionPool {
+
     Connection getConnection();
+
     boolean releaseConnection(Connection connection);
-    String getUrl();
-    String getUser();
-    String getPassword();
+
     int getSize();
 }
