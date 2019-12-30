0e5e69ec46fa8a506d8cda65decbd97bc756c38f
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-12-31 00:24:09 +0200

    Corrected misspell.

--- src/main/java/enotes/note/dao/JdbcNoteDao.java
+++ src/main/java/enotes/note/dao/JdbcNoteDao.java
@@ -24,7 +24,7 @@ public class JdbcNoteDao implements NoteDao {
 
     private static final String ADD_NEW_NOTE_SQL = "INSERT INTO notes (header, body, state, user_id) VALUES ('%s', '%s', '%s', %d);";
     private static final String SELECT_ALL_NOTES_SQL = "SELECT * FROM notes WHERE is_deleted=0;";
-    private static final String SELECT_NOTE_BY_ID_SQL = "SELECT * FROM notes WHERE id = %s AND is_deleted=0;";
+    private static final String SELECT_NOTE_BY_ID_SQL = "SELECT * FROM notes WHERE id = %d AND is_deleted=0;";
     private static final String UPDATE_NOTE_SQL = "UPDATE notes SET header = '%s', body = '%s', state = '%s' WHERE id = %s;";
     private static final String DELETE_NOTE_BY_ID_SQL = "UPDATE notes SET is_deleted=1 WHERE id = %s;";
 
