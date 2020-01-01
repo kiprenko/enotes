90a4500ca5ad4bdf48cdce843ce0ea95069ad97d
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2020-01-02 01:11:09 +0200

    Made JdbcConnectionManagerTest

--- src/test/java/enotes/db/JdbcConnectionManagerTest.java
+++ src/test/java/enotes/db/JdbcConnectionManagerTest.java
@@ -1,6 +1,29 @@
 package enotes.db;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JdbcConnectionManagerTest {
 
+    @Autowired
+    private ConnectionManager connectionManager;
 
+    @Test
+    public void mustReturnValidConnection() throws SQLException {
+        Connection connection = connectionManager.getConnection();
+        assertNotNull("Returned connection must be not null.",connection);
+        assertTrue("Returned connection must be valid.", connection.isValid(0));
+    }
 }
