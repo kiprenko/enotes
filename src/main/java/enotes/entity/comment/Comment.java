1d796625ca96727deb8615a63d33d4f94cb46b47
EKiprenko <evgeniy.kiprenko@cengage.com>
2020-02-10 13:44:49 +0200

    ENOTES-8 - removed wildcard imports.

--- src/main/java/enotes/entity/comment/Comment.java
+++ src/main/java/enotes/entity/comment/Comment.java
@@ -13,7 +13,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
 
 @Entity
 @Table(name = "comments")
--- src/main/java/enotes/entity/user/User.java
+++ src/main/java/enotes/entity/user/User.java
@@ -11,7 +11,15 @@ import lombok.Setter;
 import lombok.Singular;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
