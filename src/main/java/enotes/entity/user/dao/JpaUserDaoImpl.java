5f713d918104e8576f89344ce355850ba37a024b
EKiprenko <evgeniy.kiprenko@cengage.com>
2020-02-21 11:47:13 +0200

    ENOTES-8 - add JpaDao for comments and users.

--- src/main/java/enotes/entity/user/dao/JpaUserDaoImpl.java
+++ src/main/java/enotes/entity/user/dao/JpaUserDaoImpl.java
@@ -3,33 +3,54 @@ package enotes.entity.user.dao;
 import enotes.entity.user.User;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class JpaUserDaoImpl implements UserDao {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    @PersistenceUnit
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    private EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+
     @Override
     public List<User> findAll() {
-        return null;
+        return getEntityManager().createQuery("FROM User", User.class).getResultList();
     }
 
     @Override
     public Optional<User> find(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(getEntityManager().find(User.class, id));
     }
 
     @Override
     public void add(User entity) {
-
+        EntityManager entityManager = getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(entity);
+        entityManager.getTransaction().commit();
     }
 
     @Override
     public void update(User entity) {
-
+        getEntityManager().merge(entity);
     }
 
     @Override
     public void delete(Long id) {
-
+        EntityManager entityManager = getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.find(User.class, id));
+        entityManager.getTransaction().commit();
     }
 }
