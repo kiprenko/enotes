b7fd00e64b874b97eab251e29ba35411fb419fd8
EKiprenko <evgeniy.kiprenko@cengage.com>
2020-02-21 12:08:09 +0200

    ENOTES-8 - fixed a bug in Comment.

--- src/main/java/enotes/entity/comment/Comment.java
+++ src/main/java/enotes/entity/comment/Comment.java
@@ -1,10 +1,7 @@
 package enotes.entity.comment;
 
-import java.io.Serializable;
-
 import enotes.entity.note.Note;
 import enotes.entity.user.User;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -17,10 +14,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "comments")
@@ -40,9 +38,9 @@ public class Comment implements Serializable {
 
     private String text;
     @ManyToOne
-    @JoinTable(name = "user_id")
+    @JoinColumn(name = "user_id")
     private User user;
     @ManyToOne
-    @JoinTable(name = "note_id")
+    @JoinColumn(name = "note_id")
     private Note note;
 }
