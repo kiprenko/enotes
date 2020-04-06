3f31b17d644b013fae72edbb9964bd027cd918ed
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-04-07 01:54:49 +0300

    ENOTES-4 - moved MVC config.

--- src/main/java/enotes/config/MvcConfig.java
+++ src/main/java/enotes/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package enotes.security;
+package enotes.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
